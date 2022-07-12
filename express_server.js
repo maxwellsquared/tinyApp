8f6eb8284acd9cea0adf3a738dceeda7ba6b075b
max k <maxkuhn@gmail.com>
2022-07-12 00:32:08 -0700

    Added generateRandomString() function to express_server

--- express_server.js
+++ express_server.js
@@ -2,6 +2,9 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
+app.use(express.urlencoded({ extended: true }));
+
+
 app.set('view engine', 'ejs');
 
 const urlDatabase = {
@@ -31,6 +34,11 @@ app.get("/urls/:id", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+});
+
 
 // app.get("/urls.json", (req, res) => {
 //   res.json(urlDatabase);
@@ -39,3 +47,17 @@ app.get("/urls/:id", (req, res) => {
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
+function generateRandomString() {
+  let myChars = "abcdefghijklmnopqrstuvwxyz0123456789";
+  let randString = "";
+  let randChar = "";
+  for (let i = 0; i < 6; i++) {
+    randChar = myChars[Math.floor(Math.random() * myChars.length)];
+    if (Math.random() > 0.5) {
+      randChar = randChar.toUpperCase();
+    }
+    randString += randChar
+  }
+  return randString
+}
