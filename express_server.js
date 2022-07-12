d6652d585bc6e12319033b5cb14e95a509494917
max k <maxkuhn@gmail.com>
2022-07-11 23:36:11 -0700

    Cleaned up express_server file

--- express_server.js
+++ express_server.js
@@ -2,13 +2,14 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
+app.set('view engine', 'ejs');
+
 // const urlDatabase = {
 //   "b2xVn2": "http://www.lighthouselabs.ca",
 //   "9sm5xK": "http://www.google.com"
 // };
 
-// sets view engine to ejs
-app.set('view engine', 'ejs');
+
 
 app.get("/", (req, res) => {
   res.render('pages/index');
