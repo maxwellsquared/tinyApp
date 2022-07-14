6fc5f973e9ed1b4f7af994905f9f7247733573e4
max k <maxkuhn@gmail.com>
2022-07-14 00:14:09 -0700

    User registration works. Sort of.

--- express_server.js
+++ express_server.js
@@ -13,24 +13,47 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+const users = {
+  userRandomID: {
+    id: "userRandomID",
+    email: "user@example.com",
+    password: "purple-monkey-dinosaur",
+  },
+  user2RandomID: {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: "dishwasher-funk",
+  },
+};
+
+//
+// GET
+//
+
+
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase, username: req.cookies["username"] };
+  const templateVars = { urls: urlDatabase, user: users[req.cookies["user_id"]] };
   res.render('urls_index', templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
-  const templateVars= { username: req.cookies["username"] };
+  const templateVars= { user: users[req.cookies["user_id"]] };
   res.render("urls_new", templateVars);
 });
 
 app.get("/", (req, res) => {
-  const templateVars = { username: req.cookies["username"] };
+  const templateVars = { user: users[req.cookies["user_id"]] };
   res.render('index', templateVars);
 });
 
 app.get("/urls/:id", (req, res) => {
-  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id], username: req.cookies["username"]};
+  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id], user: users[req.cookies["user_id"]]};
   res.render("urls_show", templateVars);
+});
+
+app.get("/register", (req, res) => {
+  const templateVars = { user: users[req.cookies["user_id"]] };
+  res.render('register', templateVars);
 });
 
 app.get("/u/:id", (req, res) => {
@@ -38,11 +61,24 @@ app.get("/u/:id", (req, res) => {
   res.redirect(longURL);
 });
 
+//
+// POST
+//
+
 app.post("/urls", (req, res) => {
-  console.log(req.body); // Log the POST request body to the console
-  urlDatabase[generateRandomString()] = addHTTP(req.body.longURL);
-  console.log(req.body);
   res.redirect("urls")
+});
+
+app.post("/urls/:id/create/", (req, res) => {
+  console.log("Created", req.body)
+  urlDatabase[generateRandomString(6)] = addHTTP(req.body.longURL);
+  res.redirect("/urls/")
+});
+
+app.post("/urls/:id/update/", (req, res) => {
+  console.log("Updated it!")
+  urlDatabase[req.params.id] = addHTTP(req.body.longURL);
+  res.redirect("/urls/")
 });
 
 app.post("/urls/:id/delete/", (req, res) => {
@@ -50,39 +86,40 @@ app.post("/urls/:id/delete/", (req, res) => {
   res.redirect("/urls/")
 });
 
-app.post("/urls/:id/update/", (req, res) => {
-  urlDatabase[req.params.id] = addHTTP(req.body.longURL); // Running into trouble because I can't get the longURL back!
+app.post("/user/:id/create", (req, res) => {
+  let newID = generateRandomString(8)
+  console.log(req.body); // Log the POST request body to the console
+  users[newID] = { id: newID, email: req.body.email, password: req.body.password};
+  console.log(users);
+  res.cookie("user_id", newID);
+
   res.redirect("/urls/")
 });
 
 app.post("/login/", (req, res) => {
-  console.log("Hit login!")
-  console.log(req.body.username);
-  console.log("Cookies:", req.cookies);
-  console.log("Params:", req.params);
-  console.log("Body:", req.body);
   res.cookie("username", req.body.username);
   res.redirect("/urls/")
 });
 
 app.post("/logout/", (req, res) => {
-  console.log("Logging out!");
-  console.log("Cookies:", req.cookies);
-  console.log("Params:", req.params);
-  console.log("Body:", req.body);
-  res.clearCookie("username");
+  console.log("Logging out...");
+  res.clearCookie("user_id");
   res.redirect("/urls/")
 });
+
+// LISTEN
 
 app.listen(PORT, () => {
   console.log(`TonyApplistening on port ${PORT}!`);
 });
 
-function generateRandomString() {
+// FUNCTIONS
+
+function generateRandomString(strLength) {
   let myChars = "abcdefghijklmnopqrstuvwxyz0123456789";
   let randString = "";
   let randChar = "";
-  for (let i = 0; i < 6; i++) {
+  for (let i = 0; i < strLength; i++) {
     randChar = myChars[Math.floor(Math.random() * myChars.length)];
     if (Math.random() > 0.5) {
       randChar = randChar.toUpperCase();
@@ -93,6 +130,8 @@ function generateRandomString() {
 }
 
 const addHTTP = function(input) {
+  if (input === undefined) return input;
+  
   if (input.substring(0, 3) !== "http") {
     input = "http://" + input;
   }
